b8251bc3268e8f318286339cd2b30a430c0de0b0
ndrewh <ndrewh@users.noreply.github.com>
2020-10-05 19:20:56 -0400

    Update phaser_stuff.js

--- js/phaser_stuff.js
+++ js/phaser_stuff.js
@@ -22,10 +22,10 @@ var game = new Phaser.Game(config);
 // Used to load required images
 function preload()
 {
-    this.load.image("player", "/images/kid_friendly_player.png");
-    this.load.image("snowball", "/images/football.gif");
-    this.load.image("particle", "/images/particle.png"); 
-    this.load.image("background", "/images/yeehaw.png");
+    this.load.image("player", "images/kid_friendly_player.png");
+    this.load.image("snowball", "images/football.gif");
+    this.load.image("particle", "images/particle.png"); 
+    this.load.image("background", "images/yeehaw.png");
 }
 
 // Initialization Code Run On Game Start
