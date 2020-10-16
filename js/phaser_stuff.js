f3d29f9203680cb5d03a81423ce9799a9b0e33b5
maxdg99 <maxdg99@gmail.com>
2020-10-15 21:28:00 -0400

    Fix styling and general look of the game.

--- js/phaser_stuff.js
+++ js/phaser_stuff.js
@@ -1,73 +1,74 @@
 // Initialize Phaser Game Object
 var config =
 {
-    type: Phaser.AUTO,
-    parent: "thecanvas",
-    width: 600,
-    height: 600,
-    
-    physics: {
-        default: 'arcade',
-        arcade: {
-          // gravity: {y: 500}
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
+  type: Phaser.AUTO,
+  parent: "thecanvas",
+  width: 768,
+  height: 370,
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      // gravity: {y: 500}
     }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  }
 };
 var game = new Phaser.Game(config);
 
+var code1;
+var code2;
+
 // Used to load required images
-function preload()
-{
-    this.load.image("player", "images/kid_friendly_player.png");
-    this.load.image("snowball", "images/football.gif");
-    this.load.image("particle", "images/particle.png"); 
-    this.load.image("background", "images/yeehaw.png");
+function preload() {
+  this.load.image("player", "images/kid_friendly_player.png");
+  this.load.image("snowball", "images/football.gif");
+  this.load.image("particle", "images/particle.png");
+  this.load.image("background", "images/yeehaw.png");
 }
 
 // Initialization Code Run On Game Start
-function create()
-{
+function create() {
   let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
-  image.setScale(this.cameras.main.width / image.width, 3.25).setScrollFactor(0);
+  image.setScale(this.cameras.main.width / image.width, .4).setScrollFactor(0);
 
   document.getElementById("run-btn").onclick = startStop(this)
 
-  var code1 = CodeMirror(document.getElementById("code1"), {
-    lineNumbers: true,
-    mode: "javascript", 
-  });
-
-  var code2 = CodeMirror(document.getElementById("code2"), {
+  code1 = CodeMirror(document.getElementById("code1"), {
     lineNumbers: true,
     mode: "javascript"
   });
 
-    // When the text field changes, mirror it to the end object 
-    code1.on("change", () => {
-      let user = game.code1.getValue()
-      localStorage.setItem("code1", user);
-      console.log("Player 1 Code Saved!");
-    });
+  code2 = CodeMirror(document.getElementById("code2"), {
+    lineNumbers: true,
+    mode: "javascript"
+  });
 
-    code2.on("change", () => {
-      let user = game.code2.getValue()
-      localStorage.setItem("code2", user);
-      console.log("Player 2 Code Saved!");
-    });
+  // When the text field changes, mirror it to the end object 
+  code1.on("change", () => {
+    let user = game.code1.getValue()
+    localStorage.setItem("code1", user);
+    console.log("Player 1 Code Saved!");
+  });
 
-    makeGame(this, code1, code2);
+  code2.on("change", () => {
+    let user = game.code2.getValue()
+    localStorage.setItem("code2", user);
+    console.log("Player 2 Code Saved!");
+  });
 
-    $('#template').change(function (e) {
-      $.ajax("/js/templates/"+$('#template').val()+".js", {dataType: "text"}).done(function (data) {
-        code2.setValue(data);
-      }).fail(function (a, b, c) {
-        console.log(c);
-      })
-    })
+  makeGame(this, code1, code2);
+}
+
+function changeTemplate(template) {
+  $.ajax("/js/templates/" + template + ".js", { dataType: "text" }).done(function (data) {
+    code2.setValue(data);
+  }).fail(function (a, b, c) {
+    console.log(c);
+  })
 }
 
 function startStop(g) {
@@ -98,6 +99,5 @@ function startStop(g) {
 }
 
 // Update Code (Runs Every Frame)
-function update()
-{
+function update() {
 }
