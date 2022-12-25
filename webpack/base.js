cce186f4fbc0a10ab9095c882db69dc53485a4d2
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-12-24 19:28:36 -0500

    Update package.json and start with webpack

---
+++ webpack/base.js
@@ -0,0 +1,51 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  watchOptions: {
+    ignored: ["**/.*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        type: "asset/source",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, "../"),
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
